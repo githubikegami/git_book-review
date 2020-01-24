099752a5b864b10aff5c89deee35d24b2d7cac47
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-01-24 12:53:56 +0000

    migrate

--- db/migrate/20200124103937_create_bookreviews.rb
+++ db/migrate/20200124103937_create_bookreviews.rb
@@ -1,8 +1,12 @@
 class CreateBookreviews < ActiveRecord::Migration[5.2]
   def change
     create_table :bookreviews do |t|
-
-      t.timestamps
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.string :publisher
+      t.text :image
+      t.timestamps null: true
     end
   end
 end
--- db/migrate/20200124104347_create_lists.rb
+++ db/migrate/20200124104347_create_lists.rb
@@ -1,8 +1,7 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-
-      t.timestamps
+      t.timestamps null: true
     end
   end
 end
