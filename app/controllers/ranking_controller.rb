cefa02b6c9d11a14a50cfbdd7814fb8beb513c59
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-04-03 10:26:26 +0000

    devise.ja.ymlの更新

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,7 @@
 class RankingController < ApplicationController
 
   def index
-    product_ids = Comment.group(:bookreview_id).order('count_bookreview_id DESC').limit(5).count(:bookreview_id).keys
+    product_ids = Comment.group(:bookreview_id).order('count_bookreview_id DESC').limit(20).count(:bookreview_id).keys
     @rankings = product_ids.map { |id| Bookreview.find(id) }
   end
 
